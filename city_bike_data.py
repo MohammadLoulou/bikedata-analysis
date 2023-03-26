59c7caba183d72815a50d2cd0b23c1e71275dc62
Loulou <mohammadloulou@hotmail.com>
2023-03-27 00:06:15 +0200

    start to generate some informations

--- city_bike_data.py
+++ city_bike_data.py
@@ -5,8 +5,14 @@ class CityBikeData:
     def __init__(self, filename):
         self.df = pd.read_csv(filename)
 
-    def generate(self, filename):
-        pass
+    def generate(self):
+        dict_infos = {}
+        self.clean()
+        # dict_infos["number_ride"] = self.rides_number(self.df)
+        dict_infos["average"] = self.average_duration()
+        dict_infos["most_popular_station_start"] = self.most_popular_station_start()
+
+        return dict_infos
 
     def clean(self):
         self.df["started_at"] = pd.to_datetime(self.df["started_at"])
@@ -23,6 +29,17 @@ class CityBikeData:
             return count["docked_bikes"]
         elif "total" in bike_type:
             return self.df["rideable_types"].count()
+
+    def average_duration(self):
+        self.df["duration"] = self.df["ended_at"] - self.df["started_at"]
+        average = self.df["duration"].mean()
+        return average
+
+    def most_popular_station_start(self):
+        return self.df["start_station_name"].value_counts()[0]
+
+    def most_popular_station_start(self):
+        return self.df["end_station_name"].value_counts()[0]
 
 
 if __name__ == "__main__":
