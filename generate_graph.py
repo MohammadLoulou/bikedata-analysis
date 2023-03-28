abb04d7e04474cc33251bdcf879430660cbb75a9
Loulou <mohammadloulou@hotmail.com>
2023-03-28 15:35:12 +0200

    add of a daily descriptive graph

--- generate_graph.py
+++ generate_graph.py
@@ -1,23 +1,36 @@
 from city_bike_data import CityBikeData
 import matplotlib.pyplot as plt
 
-JC = CityBikeData("JC-202302.csv")
-
 
 class GenerateGraph:
-    def generate_start_id_graph(self):
+    def __init__(self):
+        self.city_bike = CityBikeData("JC-202302.csv")
+
+    def start_id_graph(self):
         """
         Diagramme en bars pour les start id
         """
-        start_id = CityBikeData.start_station_id()
+        start_id = self.city_bike.start_station_id()
         start_id.plt.bar()
 
         return start_id
 
-    def generate_end_id_graph(self):
+    def end_id_graph(self):
         """
         Diagramme en bars pour les end id
         """
-        end_id = CityBikeData.end_station_id()
+        end_id = self.city_bike.end_station_id()
 
         return end_id
+
+    def hourly_rentals_graph(self):
+        hours = self.city_bike.df["started_at"].dt.hour
+        plt.hist(hours, bins=24, edgecolor="black")
+        plt.xticks(range(0, 24), range(0, 24))
+        plt.xlabel("Heure de la journée")
+        plt.ylabel("Nombre de locations")
+        return plt
+
+    def most_popular_stations_graph(self):
+        pop = self.city_bike.most_popular_stations_start()
+        pop.plot(kind="bar", legend="popular stations")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from city_bike_data import CityBikeData
 from generate_graph import GenerateGraph
 
 JC = CityBikeData("JC-202302.csv")
-# JC = GenerateGraph()
+
 
 if __name__ == "__main__":
     JC.generate()
