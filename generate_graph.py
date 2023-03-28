96726e13a81bd1ae6be09100790db6fdf479ad13
HUSSEIN Adam <mh54500@gmail.com>
2023-03-28 17:35:24 +0200

    Allows us to generate graphs about the rideable type used according to the person if he is a member or casual

--- generate_graph.py
+++ generate_graph.py
@@ -1,10 +1,16 @@
 from city_bike_data import CityBikeData
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class GenerateGraph:
     def __init__(self):
         self.city_bike = CityBikeData("JC-202302.csv")
+
+    def generate(self):
+        self.city_bike.generate()
+        self.graph_member_or_casual()
+        self.rideable_type_graph()
 
     def start_id_graph(self):
         """
@@ -20,6 +26,7 @@ class GenerateGraph:
         Diagramme en bars pour les end id
         """
         end_id = self.city_bike.end_station_id()
+        end_id.plt_bar()
 
         return end_id
 
@@ -34,3 +41,24 @@ class GenerateGraph:
     def most_popular_stations_graph(self):
         pop = self.city_bike.most_popular_stations_start()
         pop.plot(kind="bar", legend="popular stations")
+
+    def rideable_type_graph(self):
+        rideable_type = self.city_bike.df["rideable_type"].value_counts()
+        rideable_type.plot(
+            kind="pie", title="rideable_type", autopct="%1.1f%%", ylabel=""
+        )
+        plt.savefig("rideable_type_graph.png")
+        plt.show()
+
+    def graph_member_or_casual(self):
+        selected_columns = ["rideable_type", "member_casual"]
+        ride_type = self.city_bike.df[selected_columns].value_counts()
+        type_pivot = (
+            ride_type.reset_index()
+            .pivot(index="rideable_type", columns="member_casual", values=0)
+            .fillna(0)
+        )
+        fig = plt.figure("figure2")
+        type_pivot.plot(kind="bar", ax=fig.gca())
+        plt.savefig("member_or_casual_graph.png")
+        plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from city_bike_data import CityBikeData
 from generate_graph import GenerateGraph
+import matplotlib.pyplot as plt
 
-JC = CityBikeData("JC-202302.csv")
 
+# JC = CityBikeData("JC-202302.csv")
+GG = GenerateGraph()
 
 if __name__ == "__main__":
-    JC.generate()
+    GG.generate()
