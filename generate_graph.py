970b054253ac8e15107e36aca758e7dca38af6df
HUSSEIN Adam <mh54500@gmail.com>
2023-03-30 18:35:45 +0200

    Displays the plots on the html template and added a method that finds out the most used slots

--- generate_graph.py
+++ generate_graph.py
@@ -12,30 +12,16 @@ class GraphGenerator:
 
     def generate(self):
         self.city_bike.generate()
-        self.graph_member_or_casual()
+        self.member_or_casual_graph()
         self.rideable_type_graph()
         self.rides_per_day_of_week_graph()
-
-    def start_id_graph(self):
-        """
-        Diagramme en bars pour les start id
-        """
-        start_id = self.city_bike.start_station_id()
-        start_id.plt.bar()
-
-        return start_id
-
-    def end_id_graph(self):
-        """
-        Diagramme en bars pour les end id
-        """
-        end_id = self.city_bike.end_station_id()
-        end_id.plt_bar()
-
-        return end_id
+        self.hourly_rides_graph()
+        self.most_popular_stations_graph()
+        self.user_type_ride_duration_graph()
 
     def hourly_rides_graph(self):
         hours = self.df["started_at"].dt.hour
+        plt.figure()
         plt.hist(hours, bins=24, edgecolor="black")
         plt.xticks(range(0, 24), range(0, 24))
         plt.xlabel("Hour of the day")
@@ -45,8 +31,8 @@ class GraphGenerator:
 
     def most_popular_stations_graph(self):
         pop = self.df["start_station_id"].value_counts().head(10)
-        pop.plot(kind="bar", legend="popular stations")
         plt.figure()
+        pop.plot(kind="bar", legend="popular stations")
         plt.xlabel("Station's name")
         plt.ylabel("Number of rides")
         plt.title(f"Most popular stations of {self.city}")
@@ -54,14 +40,15 @@ class GraphGenerator:
         plt.savefig(f"{self.city}_most_popular_stations.png")
 
     def rideable_type_graph(self):
+        plt.figure()  # create new figure
         rideable_type = self.df["rideable_type"].value_counts()
         rideable_type.plot(
             kind="pie", title="rideable_type", autopct="%1.1f%%", ylabel=""
         )
         plt.tight_layout()
-        plt.savefig("rideable_type_graph.png")
+        plt.savefig(f"{self.city}_rideable_type_graph.png")
 
-    def graph_member_or_casual(self):
+    def member_or_casual_graph(self):
         selected_columns = ["rideable_type", "member_casual"]
         ride_type = self.df[selected_columns].value_counts()
         type_pivot = (
@@ -72,13 +59,13 @@ class GraphGenerator:
         fig = plt.figure("figure2")
         type_pivot.plot(kind="bar", ax=fig.gca())
         plt.tight_layout()
-        plt.savefig(f"{ self.city }_average_ride_duration_user_type.png")
+        plt.savefig(f"{ self.city }_rideable_type_by_user_category.png")
 
     def rides_per_day_of_week_graph(self):
         self.df["day_of_week"] = self.df["started_at"].dt.dayofweek
         rides_per_day_of_week = self.df.groupby("day_of_week")["ride_id"].count()
 
-        plt.figure()
+        fig = plt.figure("figure3")
         rides_per_day_of_week.sort_index().plot(kind="bar")
         plt.xlabel("Day_of_week")
         plt.ylabel("Total Rides")
@@ -95,14 +82,15 @@ class GraphGenerator:
                 "Sunday",
             ],
         )
+
         plt.title(f"Total Rides per Day_of_week in {self.city}")
         plt.tight_layout()
         plt.savefig(f"{self.city}_total_rides_per_day_of_week.png")
 
     def user_type_ride_duration_graph(self):
-        ride_duration = self.data.groupby("member_casual")["ride_duration"].mean()
+        ride_duration = self.df.groupby("member_casual")["ride_duration"].mean()
 
-        plt.figure(figsize=())
+        plt.figure(figsize=(5, 5))
         ride_duration.plot(kind="bar")
         plt.xlabel("User Type")
         plt.ylabel("Average ride duration in minutes")
